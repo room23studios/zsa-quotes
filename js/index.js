320c1826c64926a9be12a8bb37a76d45a4a7e852
Marcel Guzik <mguzik555@gmail.com>
2018-03-02 09:01:41 +0100

    Added completely not working buttons

--- js/index.js
+++ js/index.js
@@ -1,5 +1,40 @@
-const quoteEl = document.querySelector('.quote');
-quote = fetch('https://alo-quotes.tk/api/random')
-    .then(response => response.json())
-    .then(quote => quote.quote)
-    .then(quote => { quoteEl.innerHTML = `"${quote.quote}"` });
+const hostname = 'https://alo-quotes.tk';
+let id;
+
+function updateQuote(quote) {
+    const quoteEl = document.querySelector('.quote');
+    quote.then(quote => quoteEl.innerHTML = quote.quote);
+}
+
+function fetchQuote(id) {
+    if (id !== undefined) {
+        return fetch(`${hostname}/api/quote/${id}`)
+            .then(response => response.json())
+            .then(json => json.quote)
+    }
+}
+
+function fetchRandomQuote() {
+    return fetch(`${hostname}/api/random`)
+        .then(response => response.json())
+        .then(json => {
+            id = json.quote.id;
+            return json.quote;
+        });
+}
+
+document.querySelector('.random').addEventListener('click', () => updateQuote(fetchRandomQuote()));
+document.querySelector('.prev').addEventListener('click', () => {
+    id -= 1;
+    updateQuote(fetchQuote(id));
+    console.log(id);
+});
+document.querySelector('.next').addEventListener('click', () => {
+    id += 1;
+    updateQuote(fetchQuote(id));
+    console.log(id);
+});
+
+updateQuote(fetchRandomQuote());
+
+window.id = id;
